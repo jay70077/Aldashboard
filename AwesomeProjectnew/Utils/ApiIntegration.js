a9fd4c3fac14c276da2c3243fc275ffc495226ad
Avinash <avinash@reckrut.com>
2020-01-04 16:20:39 +0530

    Training Graph with params

--- AwesomeProjectnew/Utils/ApiIntegration.js
+++ AwesomeProjectnew/Utils/ApiIntegration.js
@@ -4,32 +4,56 @@ export default {
     dashboardApi(){
         return axios({
             method: 'get',
-            url: 'http://ec2-54-86-165-209.compute-1.amazonaws.com/api/v1/dashboard'
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/dashboard'
         });
     },
 
     filterData(value){
         return axios({
             method: 'get',
-            url: 'http://ec2-54-86-165-209.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&bu='+value
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&bu='+value
         });
     },
     filterTarget(value){
         return axios({
             method: 'get',
-            url: 'http://ec2-54-86-165-209.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&target_au='+value
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&target_au='+value
         });
     },
     filterUser(value){
         return axios({
             method: 'get',
-            url: 'http://ec2-54-86-165-209.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&users='+value
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/dashboard/get-stats?sel_type=m&users='+value
         });
     },
     trainGraph(){
         return axios({
             method: 'get',
-            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/reports/get-trainings-for-id-for-graph?uid=2&month=6&year=2019'
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/reports/get/participants/trainings'
+        });
+    },
+	trainGraphWithParams(){
+        return axios({
+            method: 'get',
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/reports/get/trainings/ajax?uid=1&month=6&year=2019'
+        });
+    },
+    trainGraphF2FZoom(){
+        return axios({
+            method: 'get',
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/reports/get/trainings'
+        });
+    },
+	trainGraphF2FZoomWithParams(){
+        return axios({
+            method: 'get',
+            url: 'http://ec2-52-87-187-122.compute-1.amazonaws.com/api/v1/reports/get/participants/trainings/ajax?uid=1&month=6&year=2019'
         });
     }
+	
+	
+	
+	
+	
+	
 }
